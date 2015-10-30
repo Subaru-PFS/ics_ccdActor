c57aeea9389f0a46f213dfd0911a5616d468e1fb
Craig Loomis <cloomis@astro.princeton.edu>
2015-10-30 10:01:14 -0400

    Starting template.

---
+++ python/ccdActor/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import actorcore.Actor
+
+class OurActor(actorcore.Actor.Actor):
+    def __init__(self, name,
+                 productName=None, configFile=None,
+                 modelNames=(),
+                 debugLevel=30):
+
+        """ Setup an Actor instance. See help for actorcore.Actor for details. """
+        
+        # This sets up the connections to/from the hub, the logger, and the twisted reactor.
+        #
+        actorcore.Actor.Actor.__init__(self, name, 
+                                       productName=productName, 
+                                       configFile=configFile,
+                                       modelNames=modelNames)
+#
+# To work
+def main():
+    theActor = OurActor('ccd', productName='ccdActor')
+    theActor.run()
+
+if __name__ == '__main__':
+    main()
