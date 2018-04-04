cd4815298162c3a297c260c2ee950a435d224ac9
Craig Loomis <cloomis@astro.princeton.edu>
2018-04-04 16:15:56 -0400

    add LVDS power control

--- python/ccdActor/Commands/CcdCmd.py
+++ python/ccdActor/Commands/CcdCmd.py
@@ -42,6 +42,8 @@ class CcdCmd(object):
             ('expose', '<darks>', self.exposeDarks),
             ('setOffset', '<offset> <value>', self.setOffset),
             ('setOffsets', '<filename>', self.setOffsets),
+            ('controlLVDS', '@(on|off)', self.controlLVDS),
+            ('readCtrlWord', '', self.readCtrlWord),
         ]
 
         # Define typed command arguments for the above commands.
@@ -301,3 +303,19 @@ class CcdCmd(object):
 
         cmd.finish('text="set!"')
 
+    def controlLVDS(self, cmd):
+        """ Enable or disable the LVDS drivers to the FEE. """
+        
+        onOff = 'on' in cmd.cmd.keywords
+
+        if onOff:
+            ret = self.ccd.enableLVDS()
+        else:
+            ret = self.ccd.disableLVDS()
+
+        cmd.finish('text="last=%s set=%s"' % (ret, onOff))
+        
+    def readCtrlWord(self, cmd):
+        """ Read the PCI R_WPU_CTRL word. """
+        
+        cmd.finish('text="R_WPU_CTRL=0x%08x"' % (self.ccd.readCtrlWord()))
