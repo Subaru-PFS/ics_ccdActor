8daa246723eccd2cbffb5331f1d024cc86221228
Craig Loomis <cloomis@astro.princeton.edu>
2022-11-11 13:11:36 -0500

    Switch configs from etc/.cfg to pfs_instdata .yaml

--- python/ccdActor/Controllers/fee.py
+++ python/ccdActor/Controllers/fee.py
@@ -10,22 +10,20 @@ class fee(feeControl.FeeControl):
     def __init__(self, actor, name,
                  logLevel=logging.DEBUG):
 
+        self.actor = actor
+        self.name = name
+
         fpga = actor.controllers.get('ccd', None)
-        port = actor.config.get('fee', 'port')
+        port = actor.actorConfig['fee']['port']
         features = actor.actorConfig.get('feeFeatures', None)
 
         feeControl.FeeControl.__init__(self, fpga=fpga,
                                        port=port,
                                        features=features,
                                        logLevel=logLevel)
-        self.actor = actor
-        self.name = name
-
         self.grabStaticKeys()
 
     def grabStaticKeys(self, cmd=None):
-        ccdModel = self.actor.ccdModel.keyVarDict
-
         self.actor.bcast.inform('version_fee="%s"' % self.getCommandStatus('revision')['revision.FEE'])
 
         serialDict = self.getCommandStatus('serial')
